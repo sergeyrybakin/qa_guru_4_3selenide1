cdfc235d0e3ec84d677436f79c8135cdf18069c3
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-07 13:52:11 +0700

    Wiki should have page Soft Assertions

--- src/test/java/tests/SelenideSoftAssertionsTest.java
+++ src/test/java/tests/SelenideSoftAssertionsTest.java
@@ -2,24 +2,29 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSoftAssertionsTest
 {
     @Test
-    void verificationOfPageOpening(){
-//      Откройте страницу Selenide в Github
+    void verificationOfPageOpening()
+    {
+        //      Откройте страницу Selenide в Github
         Selenide.open("https://github.com/selenide/selenide");
 
-//      Перейдите в раздел Wiki проекта
+        //      Перейдите в раздел Wiki проекта
         $("span[data-content=Wiki]").click();
+        $(".markdown-body h1").shouldHave(Condition.text("Welcome to the selenide wiki!"));
 
-//        - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-//
-//                - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-        sleep(3000);
+        //      Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $$(".markdown-body ul").filterBy(Condition.text("Soft Assertions")).shouldHave(CollectionCondition.size(1));
+
+        //
+        //                - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        sleep(2000);
     }
 }
