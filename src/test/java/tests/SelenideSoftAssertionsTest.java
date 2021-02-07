22d01b8816ee489cced0b24fafe420a5319e6805
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-07 18:34:01 +0700

    add startMaximized, add verifications of texts

--- src/test/java/tests/SelenideSoftAssertionsTest.java
+++ src/test/java/tests/SelenideSoftAssertionsTest.java
@@ -1,30 +1,46 @@
 package tests;
 
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSoftAssertionsTest
 {
+    @BeforeAll
+    static void setUp()
+    {
+        Configuration.startMaximized = true;
+    }
+
     @Test
     void verificationOfPageOpening()
     {
-        //      Откройте страницу Selenide в Github
-        Selenide.open("https://github.com/selenide/selenide");
+        //Откройте страницу Selenide в Github
+        Selenide.open("https://github.com");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li a").filterBy(Condition.exactText("selenide/selenide")).first().click();
 
-        //      Перейдите в раздел Wiki проекта
+        //Перейдите в раздел Wiki проекта
         $("span[data-content=Wiki]").click();
         $(".markdown-body h1").shouldHave(Condition.text("Welcome to the selenide wiki!"));
 
-        //      Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $$(".markdown-body ul").filterBy(Condition.text("Soft Assertions")).shouldHave(CollectionCondition.size(1));
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        //Откройте страницу SoftAssertions
+        $$(".markdown-body li a").filterBy(Condition.text("Soft assertions")).shouldHaveSize(1).first().click();
+        $("#wiki-wrapper h1").shouldHave(Condition.text("SoftAssertions"), Duration.ofMillis(6000));
 
-        //
-        //                - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        //проверьте что внутри есть пример кода для JUnit5
+        $$("#wiki-body li")
+                .shouldHave(CollectionCondition.itemWithText("Using JUnit5 extend test class:"), Duration.ofSeconds(6));
+
         sleep(2000);
     }
 }
