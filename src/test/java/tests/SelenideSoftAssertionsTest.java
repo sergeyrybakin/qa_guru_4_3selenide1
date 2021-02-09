9494895d4e63d711d2f106277e16ebc8649f0126
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-09 20:18:16 +0700

    исправление: 1. Condition.exactText condition можно импортировать статически дополнительно все условия импортировал статически.

--- src/test/java/tests/SelenideSoftAssertionsTest.java
+++ src/test/java/tests/SelenideSoftAssertionsTest.java
@@ -5,11 +5,12 @@ import java.time.Duration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.CollectionCondition.itemWithText;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSoftAssertionsTest
@@ -26,20 +27,20 @@ public class SelenideSoftAssertionsTest
         //Откройте страницу Selenide в Github
         Selenide.open("https://github.com");
         $("[name=q]").setValue("selenide").pressEnter();
-        $$("ul.repo-list li a").filterBy(Condition.exactText("selenide/selenide")).first().click();
+        $$("ul.repo-list li a").filterBy(exactText("selenide/selenide")).first().click();
 
         //Перейдите в раздел Wiki проекта
         $("span[data-content=Wiki]").click();
-        $(".markdown-body h1").shouldHave(Condition.text("Welcome to the selenide wiki!"));
+        $(".markdown-body h1").shouldHave(text("Welcome to the selenide wiki!"));
 
         //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         //Откройте страницу SoftAssertions
-        $$(".markdown-body li a").filterBy(Condition.text("Soft assertions")).shouldHaveSize(1).first().click();
-        $("#wiki-wrapper h1").shouldHave(Condition.text("SoftAssertions"), Duration.ofMillis(6000));
+        $$(".markdown-body li a").filterBy(text("Soft assertions")).shouldHaveSize(1).first().click();
+        $("#wiki-wrapper h1").shouldHave(text("SoftAssertions"), Duration.ofMillis(6000));
 
         //проверьте что внутри есть пример кода для JUnit5
         $$("#wiki-body li")
-                .shouldHave(CollectionCondition.itemWithText("Using JUnit5 extend test class:"), Duration.ofSeconds(6));
+                .shouldHave(itemWithText("Using JUnit5 extend test class:"), Duration.ofSeconds(6));
 
         sleep(2000);
     }
